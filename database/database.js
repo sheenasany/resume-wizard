24404ca29f7a0a671fd239eed87ff7bd3351ce4c
grumblefoot <rowanosmon@gmail.com>
2023-09-16 11:05:55 -0700

    updated database.js

--- database/database.js
+++ database/database.js
@@ -5,50 +5,19 @@ class Database {
     this.db = new sqlite3.Database(dbPath);
   }
 
-  query(queryString, params, callback) {
-    this.db.all(queryString, params, (err, rows) => {
-      if (err) {
-        console.error(err);
-        callback(err, null);
-        return;
-      }
+  // Method to query the database
+  query(queryString, params) {
+    return new Promise((resolve, reject) => {
+      this.db.all(queryString, params, (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
 
-      callback(null, rows);
+        resolve(rows);
+      });
     });
-  }
-
-  // Method to insert data
-  insert(tableName, columns, values, callback) {
-    const query = `INSERT INTO ${tableName} (${columns.join(', ')}) VALUES (${values.map(() => '?').join(', ')})`;
-    this.db.run(query, values, function(err) {
-      if (err) {
-        console.error(err);
-        callback(err);
-        return;
-      }
-
-      callback(null, this.lastID); // Return the ID of the inserted row
-    });
-  }
-
-  // Method to delete data
-  delete(tableName, condition, params, callback) {
-    const query = `DELETE FROM ${tableName} WHERE ${condition}`;
-    this.db.run(query, params, function(err) {
-      if (err) {
-        console.error(err);
-        callback(err);
-        return;
-      }
-
-      callback(null, this.changes); // Return the number of affected rows
-    });
-  }
-
-  // Method for a simple join operation
-  join(query, params, callback) {
-    this.query(query, params, callback);
-  }
+}
 
   // Method to close the database connection
   close(callback) {
